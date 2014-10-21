7051215af72d69b7baf649eacce43e4708850ba1
Sourav Ray <me@raysourav.com>
2014-10-21 18:06:50 +0530

    update data type

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -26,7 +26,7 @@ type Product struct {
 	Name        string        `json:"name" bson:"name"`
 	Img         string        `json:"img"  bson:"img"`
 	Description string        `json:"description" bson:"description"`
-	Price       int64         `json:"price"  bson:"price"`
+	Price       string         `json:"price"  bson:"price"`
 	PriceUnit   string        `json:"price_unit"  bson:"price_unit"`
 }
 
